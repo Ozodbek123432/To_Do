3948960141980ac71acb1fe49fa609df2a98c519
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-20 19:06:34 +0500

    To_Do

--- to_do_beckend/urls.py
+++ to_do_beckend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from .views import TodoView,ToDoList,ToDoDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('list/', TodoView.as_view(),name="malumoaltni korish"),
     path('create/', ToDoList.as_view(),name='malumoaltarni yaraytish'),
     path('detail/<int:pk>/`x', ToDoDetail.as_view(),name = "malumoatlarni ochirish"),
--- to_do_control/urls.py
+++ to_do_control/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from to_do_beckend.views import *
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('maenu/', include('to_do_beckend.urls')),
 ]
